6cfdb6900364adb5f4c4cbd9be2bbf2fb4d0a24b
juno-junho <ssmm0205@naver.com>
2022-09-02 18:47:50 +0900

    Sep 02nd, 2022, spring core project finished

--- inflearn/spring_concept_01/src/main/java/hello/core/common/MyLogger.java
+++ inflearn/spring_concept_01/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -9,26 +10,32 @@ import java.util.UUID;
 
 @Component
 @Scope(value = "request")
+//@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
+
 
     private String uuid;
     private String requestURL;
 
+    // requestURL은 bean이 생성되는 시점에 알 수 없기때문에 setter로 외부에서 받음.
     public void setRequestURL(String requestURL) {
         this.requestURL = requestURL;
     }
-    public void log(String message){
-        System.out.println("[" + uuid + "] ["+requestURL+"] " + message);
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "] [" + requestURL + "] " + message);
     }
 
     @PostConstruct
-    public void init(){
-        uuid = UUID.randomUUID().toString();
+    public void init() {
+        // 고객요청 들어올때. 호출 필요하다 달라하면 호출
+        uuid = UUID.randomUUID().toString(); // 겹치지 않는다. (다른 http 요청과 구분 할 수 있다)
         System.out.println("[" + uuid + "] request scope bean create : " + this);
     }
 
     @PreDestroy
-    public void close(){
+    // 고객요청이 서버에서 빠져나가면 소멸되기전 호출
+    public void close() {
         System.out.println("[" + uuid + "] request scope bean close : " + this);
     }
 }
--- inflearn/spring_concept_01/src/main/java/hello/core/web/LogDemoController.java
+++ inflearn/spring_concept_01/src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -9,19 +10,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
-@RequiredArgsConstructor
+@RequiredArgsConstructor //autowired
 public class LogDemoController {
-    private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
 
-    @RequestMapping("log-demo")
-    @ResponseBody
-    public String logDemo(HttpServletRequest request){
-        String requestURL = request.getRequestURL().toString();
+    private final LogDemoService logDemoService;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
+    @RequestMapping("/log-demo")
+    @ResponseBody   // 화면이 없기 때문에 문자 바로 반환(http 메세지에 데이터를 바로 출력하는 기능)
+   public String logDemo(HttpServletRequest request) throws InterruptedException { //고객 요청 정보를 받을 수 있다.
+        String requestURL = request.getRequestURL().toString(); // 고객이 어떤 url로 요청했는지 알 수 있다.
+
+        MyLogger myLogger = myLoggerProvider.getObject();
+
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
-        logDemoService.logic("testId");
+        Thread.sleep(1000);
+        logDemoService.logic("testId"); // service에서도 호출해 보는 것.
         return "OK";
     }
 }
