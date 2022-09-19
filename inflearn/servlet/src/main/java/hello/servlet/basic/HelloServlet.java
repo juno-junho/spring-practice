54c9e32a2baed4c1289494c0700c379840f5394b
juno-junho <ssmm0205@naver.com>
2022-09-19 22:25:40 +0900

    Sep 19th, 2022, inflearn spring mvc started

--- inflearn/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ inflearn/servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -20,7 +20,7 @@ public class HelloServlet extends HttpServlet {
 
         response.setContentType("text/plain");
         response.setCharacterEncoding("utf-8");
-        response.getWriter().write("hello" + username);
+        response.getWriter().write("hello " + username);
 
     }
 }
