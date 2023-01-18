fdacb162d523d94b86386fe97eab0cf088802044
juno-junho <ssmm0205@naver.com>
2023-01-18 16:31:43 +0900

    feat: 타임리프 -  주석

--- inflearn/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ inflearn/thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -115,6 +115,13 @@ public class BasicController {
         addUsers(model);
         return "basic/condition";
     }
+
+    @GetMapping("/comments")
+    public String comments(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/comments";
+    }
+
     @Data
     static class User {
         private String username;
