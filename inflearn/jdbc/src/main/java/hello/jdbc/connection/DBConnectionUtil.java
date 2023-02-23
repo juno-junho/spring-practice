01c1c7629595217460caf45d4d5a39e4a431fe36
juno-junho <ssmm0205@naver.com>
2023-02-23 13:48:00 +0900

    feat: 자바 표준 인터페이스를 활용한 db 연결

---
+++ inflearn/jdbc/src/main/java/hello/jdbc/connection/DBConnectionUtil.java
@@ -0,0 +1,24 @@
+package hello.jdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class DBConnectionUtil {
+
+    // jdbc 표준 인터페이스가 제공하는 그 커넥션
+    public static Connection getConnection() {
+        try {
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            log.info("get connection={}, class={}", connection, connection.getClass());
+            return connection;
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
