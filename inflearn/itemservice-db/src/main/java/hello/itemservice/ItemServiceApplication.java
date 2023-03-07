8beb98497d2ad76c8c152236f8617d87bcee55ec
juno-junho <ssmm0205@naver.com>
2023-03-07 22:45:57 +0900

    feat: 스프링 DB 2편 - 테스트 접근 기술 (embedded mode db)

--- inflearn/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ inflearn/itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,14 +2,19 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 //@Import(MemoryConfig.class)
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -22,6 +27,18 @@ public class ItemServiceApplication {
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");	// jvm내에 db 만들고 거기에 data 쌓는다.
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 /**
  * NamedParameterJdbcTemplate 사용 권장하긴 하지만 간단간단한것 일때는 JdbcTemplate 사용해도 되지만 순서 조심해야한다.
