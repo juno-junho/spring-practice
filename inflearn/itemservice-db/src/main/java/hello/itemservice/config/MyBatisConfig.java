e784d8db5c72a6b58b31876f9e5ad2eba1ab8102
juno-junho <ssmm0205@naver.com>
2023-03-10 19:44:02 +0900

    feat: 스프링 DB 2편 - Mybatis 학습

---
+++ inflearn/itemservice-db/src/main/java/hello/itemservice/config/MyBatisConfig.java
@@ -0,0 +1,27 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.mybatis.ItemMapper;
+import hello.itemservice.repository.mybatis.MyBatisItemRepository;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@RequiredArgsConstructor
+public class MyBatisConfig {
+    private final ItemMapper itemMapper;
+    // datasource 경우 mybatis module이 datasource나 tx manager 다 읽어서 mapping 해준다.
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    @Bean
+    public ItemRepository itemRepository() {
+        return new MyBatisItemRepository(itemMapper);
+    }
+}
