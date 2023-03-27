86c1958f97f2dc0d01986c205dd566d2e13dd7ae
juno-junho <ssmm0205@naver.com>
2023-03-27 23:38:21 +0900

    feat(validation): 오류코드와 메세지 처리 5,6

--- inflearn/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ inflearn/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -1,10 +1,8 @@
 package hello.itemservice.validation;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.ObjectError;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
